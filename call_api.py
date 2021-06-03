6ffe33d37e729206f1c6b723503351067c2490b1
David Li <davidli012345@gmail.com>
2021-06-02 20:46:32 -0700

    Update call_api.py

--- call_api.py
+++ call_api.py
@@ -1,9 +1,11 @@
 import requests
 from config import COMPANIES
 import os
+import time
 
 base_url = os.environ.get("DLI_LINKEDIN_API")
 for company in COMPANIES:
     url = f"{base_url}/company/{company}"
     x = requests.get(url)
+    time.sleep(120)
     print(x)
