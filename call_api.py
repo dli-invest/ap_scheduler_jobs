a743c2b83079e1913e21e739b34d299a4d9999fd
David Li <davidli012345@gmail.com>
2021-06-03 21:46:47 -0700

    Update call_api.py

--- call_api.py
+++ call_api.py
@@ -7,5 +7,5 @@ base_url = os.environ.get("DLI_LINKEDIN_API")
 for company in COMPANIES:
     url = f"{base_url}/company/{company}"
     x = requests.get(url)
-    time.sleep(120)
+    time.sleep(60*10)
     print(x)
